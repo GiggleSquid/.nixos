2cd916625a3552c58872f9ab1605e6f0549c30be
GiggleSquid <jack.connors@protonmail.com>
2024-03-30 23:48:22 +0000

    fix: remove cloudflare ntp as an upstream
    it's stratum 4, if it's ever the source I don't wan't my systems being stratum 5 if I can help it

--- nix/servers/nixosProfiles/chrony/default.nix
+++ nix/servers/nixosProfiles/chrony/default.nix
@@ -6,9 +6,8 @@
       enable = true;
       extraFlags = [ "-x" ];
       extraConfig = ''
-        pool uk.pool.ntp.org iburst maxsources 4 minstratum 2 xleave
+        pool uk.pool.ntp.org iburst maxsources 5 minstratum 2 xleave
         pool time.nist.gov maxsources 2 minstratum 2 xleave
-        server time.cloudflare.com xleave
         allow 10.0.0.0/8
       '';
     };
