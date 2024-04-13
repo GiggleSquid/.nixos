dd2dbb1cc30cbb2f30e8e5b7ee70347a45881e39
GiggleSquid <jack.connors@protonmail.com>
2024-04-13 04:08:15 +0100

    feat: set middlemouse scroll in librewolf (god how I've missed it)

--- nix/squid/homeProfiles/browser/default.nix
+++ nix/squid/homeProfiles/browser/default.nix
@@ -5,6 +5,8 @@
       "identity.fxaccounts.enabled" = true;
       "privacy.clearOnShutdown.history" = false;
       "privacy.clearOnShutdown.downloads" = false;
+      "middlemouse.paste" = false;
+      "general.autoScroll" = true;
     };
   };
 }
