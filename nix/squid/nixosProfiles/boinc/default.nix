b0896247b9aa629f8473067b384156a90c0aaa31
GiggleSquid <jack.connors@protonmail.com>
2024-11-07 14:13:31 +0000

    feat: add folding@home

--- nix/squid/nixosProfiles/boinc/default.nix
+++ nix/squid/nixosProfiles/boinc/default.nix
@@ -3,13 +3,21 @@ let
   inherit (inputs) nixpkgs;
 in
 {
-  services.boinc = {
-    enable = true;
-    extraEnvPackages = with nixpkgs; [
-      virtualbox
-      libglvnd
-      brotli
-      ocl-icd
-    ];
+  services = {
+    boinc = {
+      enable = true;
+      extraEnvPackages = with nixpkgs; [
+        virtualbox
+        libglvnd
+        brotli
+        ocl-icd
+      ];
+    };
+    foldingathome = {
+      enable = true;
+      team = 226804;
+      user = null;
+      extraArgs = [ ];
+    };
   };
 }
