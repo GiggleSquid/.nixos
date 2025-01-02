9d8972b4b5a95813e2c36e1bbd25d40df6390a5b
GiggleSquid <jack.connors@protonmail.com>
2025-01-02 22:26:14 +0000

    chore: update thatferretblog

--- nix/toolchain/packages/hugo-website-thatferretblog/package.nix
+++ nix/toolchain/packages/hugo-website-thatferretblog/package.nix
@@ -9,8 +9,8 @@ stdenv.mkDerivation {
     fetchSubmodules = true;
     owner = "GiggleSquid";
     repo = "thatferretblog";
-    rev = "468bb6a9fe74fad93cef83b0b4c0d8d605d974d9";
-    hash = "sha256-1oidZOvD8InHDgduzyHVLfGg6Pjm7GRzn3dIVp1pzqA=";
+    rev = "0b3c422a48937ce08ab1d6ecaa67a96e4c5bb534";
+    hash = "sha256-SuaYynceba5A+IYRzKGtk8J6rtQc043BQo2EtJS0nQg=";
   };
   nativeBuildInputs = [ hugo ];
   phases = [
