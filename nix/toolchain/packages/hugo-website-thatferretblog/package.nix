e4a655dd4fcf364bdc364fdd95d5a67eade59949
GiggleSquid <jack.connors@protonmail.com>
2025-01-03 11:32:45 +0000

    fix: support enableGitInfo in hugo
    this uses deepClone which is problematic in nix. deepClone is inherently
    non deterministic and can be a pita. see:
    https://github.com/NixOS/nixpkgs/issues/8567

--- nix/toolchain/packages/hugo-website-thatferretblog/package.nix
+++ nix/toolchain/packages/hugo-website-thatferretblog/package.nix
@@ -1,22 +1,28 @@
 {
   stdenv,
   fetchFromGitHub,
+  git,
   hugo,
 }:
 stdenv.mkDerivation {
   name = "hugo-website-thatferretblog";
   src = fetchFromGitHub {
     fetchSubmodules = true;
+    deepClone = true;
     owner = "GiggleSquid";
     repo = "thatferretblog";
     rev = "0b3c422a48937ce08ab1d6ecaa67a96e4c5bb534";
     hash = "sha256-SuaYynceba5A+IYRzKGtk8J6rtQc043BQo2EtJS0nQg=";
   };
-  nativeBuildInputs = [ hugo ];
+  nativeBuildInputs = [
+    hugo
+    git
+  ];
   phases = [
     "unpackPhase"
     "buildPhase"
   ];
+
   buildPhase = ''
     hugo build --minify -s . -d "$out"
   '';
