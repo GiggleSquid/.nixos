854e522c5c8ff83b01226dae85ec505eeeaa9d26
GiggleSquid <jack.connors@protonmail.com>
2025-01-09 00:06:33 +0000

    feat: use buildNpmPackage for thatferretblog as we now use npm upstream

--- nix/toolchain/packages/hugo-website-thatferretblog/package.nix
+++ nix/toolchain/packages/hugo-website-thatferretblog/package.nix
@@ -1,29 +1,39 @@
 {
-  stdenv,
+  buildNpmPackage,
   fetchFromGitHub,
+  nodejs_22,
   git,
   hugo,
 }:
-stdenv.mkDerivation {
+buildNpmPackage {
   name = "hugo-website-thatferretblog";
+
   src = fetchFromGitHub {
     fetchSubmodules = true;
     deepClone = true;
     owner = "GiggleSquid";
     repo = "thatferretblog";
-    rev = "7730f2689d4b8eb512393a0a97ab838ce4fb0c7f";
-    hash = "sha256-P+UV8NlWST1DTkPIFGCNUlMOoFvpMU3fbwovo9t0nOs=";
+    rev = "95dd238df39101a1673f7b2d5053fb955e8ed7b8";
+    hash = "sha256-4sImC8HkxaiKXzH+63Hrn53oXVw52UDgo7fNNEurSJc=";
   };
+
+  npmDepsHash = "sha256-ZK1sY7m593lzz/idO2Twcao3klwd611UDFBiALyOW2M=";
+
+  dontNpmInstall = true;
+
+  nodejs = nodejs_22;
+
   nativeBuildInputs = [
     hugo
     git
   ];
-  phases = [
-    "unpackPhase"
-    "buildPhase"
-  ];
 
   buildPhase = ''
-    hugo build --minify -s . -d "$out"
+    # Needs fix
+    # We run hugo first to generate the hugo_stats.json.
+    # Not entirely sure why this is needed or if there's a better method.
+    # AFAIK the second invocation should handle it just fine on its own.
+    hugo
+    hugo --minify -s . -d "$out"
   '';
 }
