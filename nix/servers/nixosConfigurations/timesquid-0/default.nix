d5ac087a9a8ded32bddba2d5548f313a0d7d99bc
GiggleSquid <jack.connors@protonmail.com>
2025-02-01 16:47:08 +0000

    refactor: remove more uses of machinProfile in timesquid-0

--- nix/servers/nixosConfigurations/timesquid-0/default.nix
+++ nix/servers/nixosConfigurations/timesquid-0/default.nix
@@ -5,7 +5,7 @@
 }:
 let
   inherit (inputs) rpi nixpkgs self;
-  inherit (cell) machineProfiles hardwareProfiles serverSuites;
+  inherit (cell) hardwareProfiles serverSuites;
   inherit (inputs.cells.squid) nixosSuites homeSuites;
   lib = nixpkgs.lib // builtins;
   hostName = "timesquid-0";
@@ -42,11 +42,42 @@ in
     };
   };
 
+  environment.systemPackages = with nixpkgs; [
+    raspberrypi-eeprom
+    libraspberrypi
+    pps-tools
+    gpsd
+  ];
+
+  services.gpsd = {
+    enable = true;
+    nowait = true;
+    devices = [
+      "/dev/ttyAMA0"
+      "/dev/pps0"
+    ];
+    extraArgs = [
+      "-r"
+      "-s"
+      "115200"
+    ];
+  };
+
+  users.users.chrony = {
+    extraGroups = [ "gpsd" ];
+  };
+
+  systemd.services."serial-getty@ttyAMA0".enable = false;
+
+  services.udev.extraRules = ''
+    SUBSYSTEM=="tty", KERNEL=="ttyAMA0", OWNER="root", GROUP="gpsd", MODE="0660"
+    SUBSYSTEM=="pps", KERNEL=="pps0", OWNER="root", GROUP="gpsd", MODE="0660"
+  '';
+
   imports =
     let
       profiles = [
         hardwareProfiles.servers-rpi
-        machineProfiles.timesquid-0
       ];
       suites =
         with serverSuites;
