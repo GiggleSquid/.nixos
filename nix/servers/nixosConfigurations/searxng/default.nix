65bae3737e7b29a8d8b4528e6aa5f56a581fc765
GiggleSquid <jack.connors@protonmail.com>
2025-02-12 04:22:02 +0000

    feat: update searxng service to use new pkg and module (and set some more options)

--- nix/servers/nixosConfigurations/searxng/default.nix
+++ nix/servers/nixosConfigurations/searxng/default.nix
@@ -7,6 +7,7 @@ let
   inherit (inputs) common nixpkgs self;
   inherit (cell) hardwareProfiles serverSuites;
   inherit (inputs.cells.squid) nixosSuites homeSuites;
+  inherit (inputs.cells.toolchain) pkgs;
   lib = nixpkgs.lib // builtins;
   hostName = "search";
 in
@@ -31,23 +32,62 @@ in
   services = {
     searx = {
       enable = true;
+      package = pkgs.searxng;
       redisCreateLocally = true;
       environmentFile = config.sops.secrets."searxng_env_vars".path;
+      runInUwsgi = true;
+      uwsgiConfig = {
+        http = ":8080";
+      };
       settings = {
         search = {
           safe_search = 0;
           autocomplete = "duckduckgo";
+          favicon_resolver = "duckduckgo";
+          default_lang = "en";
         };
         server = {
           base_url = "https://search.gigglesquid.tech";
           port = 8080;
           bind_address = "0.0.0.0";
           secret_key = "@SEARX_SECRET_KEY@";
+          method = "GET";
+          public_instance = false;
           image_proxy = true;
           limiter = false;
         };
+        engines = lib.mapAttrsToList (name: value: { inherit name; } // value) {
+          "nixos wiki".disabled = false;
+          "codeberg".disabled = false;
+          "gitea.com".disabled = false;
+          "gitlab".disabled = false;
+          "caddy.community".disabled = false;
+          "npm".disabled = false;
+          "crates.io".disabled = false;
+          "1337x".disabled = false;
+          "nyaa".disabled = false;
+          "annas archive".disabled = false;
+          "reddit".disabled = false;
+          "duckduckgo images".disabled = false;
+          "duckduckgo videos".disabled = false;
+          "wikibooks".disabled = false;
+          "wikisource".disabled = false;
+          "wikispecies".disabled = false;
+        };
       };
-      limiterSettings = { };
+      faviconsSettings = {
+        favicons = {
+          cfg_schema = 1;
+          cache = {
+            db_url = "/run/searx/faviconcache.db";
+            LIMIT_TOTAL_BYTES = 2147483648;
+            HOLD_TIME = 2592000;
+            BLOB_MAX_BYTES = 40960;
+            MAINTENANCE_MODE = "auto";
+            MAINTENANCE_PERIOD = 3600;
+          };
+        };
+      };
     };
   };
 
@@ -58,6 +98,7 @@ in
         with serverSuites;
         lib.concatLists [
           nixosSuites.server
+          searxng
         ];
     in
     lib.concatLists [
