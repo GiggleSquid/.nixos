694407b090b5136de0eae41905c533e69c7359d7
GiggleSquid <jack.connors@protonmail.com>
2025-07-19 20:54:08 +0100

    feat: add nh to devshell

--- nix/repo/devshells.nix
+++ nix/repo/devshells.nix
@@ -16,6 +16,18 @@ lib.mapAttrs (_: dev.mkShell) {
 
     imports = [ bootstrap ];
 
+    env = [
+      {
+        name = "NH_FLAKE";
+        eval = "$PRJ_ROOT";
+      }
+    ];
+
+    packages = with nixpkgs; [
+      ssh-to-age
+      nh
+    ];
+
     commands =
       let
         mkCategory = category: attrset: attrset // { inherit category; };
@@ -26,28 +38,25 @@ lib.mapAttrs (_: dev.mkShell) {
         (builtins.map hexagon [
           { package = colmena.packages.colmena; }
           { package = nixpkgs.sops; }
-          { package = nixpkgs.ssh-to-age; }
-          { package = nixpkgs.transcrypt; }
-          { package = nixpkgs.openssl; }
           {
             name = "larva";
             help = "Write a minimal proxmox lxc image to disk";
             command = ''
-              nixos-generate --flake $PRJ_ROOT#larva -f proxmox-lxc $@
+              nixos-generate --flake "$PRJ_ROOT"#larva -f proxmox-lxc "$@"
             '';
           }
           {
             name = "pupae";
             help = "Write a minimal proxmox vm image to disk";
             command = ''
-              nixos-generate --flake $PRJ_ROOT#pupae -f proxmox $@
+              nixos-generate --flake "$PRJ_ROOT"#pupae -f proxmox "$@"
             '';
           }
           {
             name = "nymph4";
             help = "Write a minimal rpi4 sd image to disk";
             command = ''
-              nixos-generate --flake $PRJ_ROOT#nymph4 -f sd-aarch64 --system aarch64-linux $@
+              nixos-generate --flake "$PRJ_ROOT"#nymph4 -f sd-aarch64 --system aarch64-linux "$@"
             '';
           }
           {
@@ -55,45 +64,50 @@ lib.mapAttrs (_: dev.mkShell) {
             help = "Convert ssh ed25519 pub key (from ssh-keyscan) to age";
             command = ''
               read -p "Remote machine IP/domain: " remote
-              ssh-keyscan -qt ed25519 $remote | ssh-to-age $@
+              ssh-keyscan -qt ed25519 $remote | ssh-to-age "$@"
             '';
           }
         ])
         (builtins.map nix [
           {
-            name = "sw";
-            help = "Switch configurations";
-            command = "sudo nixos-rebuild switch --upgrade --flake $PRJ_ROOT $@";
-          }
-          {
             name = "boot";
             help = "Switch boot configuration";
-            command = "sudo nixos-rebuild boot --upgrade --flake $PRJ_ROOT $@";
+            command = ''nh os boot "$@"'';
+          }
+          {
+            name = "build";
+            help = "Build configuration";
+            command = ''nh os build "$@"'';
+          }
+          {
+            name = "sw";
+            help = "Switch configurations";
+            command = ''nh os switch "$@"'';
           }
           {
             name = "vm";
             help = "Test configuration in a VM";
-            command = "sudo nixos-rebuild build-vm --flake $PRJ_ROOT $@";
+            command = ''nh os build-vm "$@"'';
           }
           {
             name = "test";
             help = "Test configuration";
-            command = "sudo nixos-rebuild test --flake $PRJ_ROOT $@";
+            command = ''nh os test "$@"'';
           }
           {
             name = "update";
             help = "Update flake";
-            command = "sudo nix-channel --update && nix flake update --flake $PRJ_ROOT $@";
+            command = ''nix flake update --flake $PRJ_ROOT "$@"'';
           }
           {
             name = "check";
             help = "Check flake";
-            command = "nix flake check $PRJ_ROOT $@";
+            command = ''nix flake check $PRJ_ROOT "$@"'';
           }
           {
             name = "gc";
             help = "Garbage collection";
-            command = "sudo nix store gc -v $@";
+            command = ''nh clean "$@"'';
           }
         ])
       ];
