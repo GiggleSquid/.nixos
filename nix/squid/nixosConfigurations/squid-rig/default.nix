dc9c0440e4f3fcbe8ad4afbfdc947f095443fe9f
GiggleSquid <jack.connors@protonmail.com>
2025-07-19 20:57:58 +0100

    chore: cleanup unused wg config from squid-rig

--- nix/squid/nixosConfigurations/squid-rig/default.nix
+++ nix/squid/nixosConfigurations/squid-rig/default.nix
@@ -26,63 +26,7 @@ in
   };
 
   systemd.network = {
-    # netdevs = {
-    #   "10-wg0" = {
-    #     netdevConfig = {
-    #       Kind = "wireguard";
-    #       Name = "wg0";
-    #       MTUBytes = "1300";
-    #     };
-    #     wireguardConfig = {
-    #       PrivateKeyFile = "${config.sops.secrets."protonvpn/squid-rig/pk".path}";
-    #       ListenPort = 51820;
-    #       FirewallMark = 34952;
-    #       RouteTable = "off";
-    #     };
-    #     wireguardPeers = [
-    #       {
-    #         PublicKey = "kNPJPSh9cam56piHoWP3ZVkWRgvgcuspf2X6IXhiZVU=";
-    #         AllowedIPs = [
-    #           "::/0"
-    #         ];
-    #         Endpoint = "[2a02:6ea0:1a01:5261::10]:51820";
-    #       }
-    #     ];
-    #   };
-    # };
     networks = {
-      # "10-wg0" = {
-      #   matchConfig.Name = "wg0";
-      #   address = [
-      #     "fdca:45ca:6140::10:2/128"
-      #   ];
-      #   dns = config.systemd.network.networks."10-lan".dns;
-      #   routingPolicyRules = [
-      #     {
-      #       Family = "ipv6";
-      #       SuppressPrefixLength = 0;
-      #       Priority = 999;
-      #       Table = "main";
-      #     }
-      #     {
-      #       Family = "ipv6";
-      #       FirewallMark = 34952;
-      #       InvertRule = true;
-      #       Table = 1000;
-      #       Priority = 1000;
-      #     }
-      #     {
-      #       To = "${ipv6Prefix}";
-      #       Priority = 10;
-      #     }
-      #   ];
-      #   routes = [
-      #     {
-      #       Gateway = "::";
-      #       Table = 1000;
-      #     }
-      #   ];
-      # };
       "10-lan" = {
         matchConfig.Name = "eno1";
         ipv6AcceptRAConfig = {
@@ -94,15 +38,6 @@ in
         gateway = [
           "10.10.0.1"
         ];
-      };
-    };
-  };
-
-  sops = {
-    defaultSopsFile = "${self}/sops/squid-rig.yaml";
-    secrets = {
-      "protonvpn/squid-rig/pk" = {
-        owner = "systemd-network";
       };
     };
   };
