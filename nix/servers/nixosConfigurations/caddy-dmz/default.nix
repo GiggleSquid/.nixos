67411cde5bfb5be6353493500a264a4ede9c3d8f
GiggleSquid <jack.connors@protonmail.com>
2025-08-07 22:46:44 +0100

    feat: caddy-dmz rename thatferret.blog to origin.thatferret.blog

--- nix/servers/nixosConfigurations/caddy-dmz/default.nix
+++ nix/servers/nixosConfigurations/caddy-dmz/default.nix
@@ -178,7 +178,7 @@ in
         #       }
         #     '';
         # };
-        "thatferret.blog" = {
+        "origin.thatferret.blog" = {
           extraConfig = # caddyfile
             ''
               import bunny_acme_settings
