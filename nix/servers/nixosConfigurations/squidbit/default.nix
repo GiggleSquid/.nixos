51c69074ab89ef51b7d8bc737d667e9d22b4e5a4
GiggleSquid <jack.connors@protonmail.com>
2025-08-07 22:50:13 +0100

    style: remove newline

--- nix/servers/nixosConfigurations/squidbit/default.nix
+++ nix/servers/nixosConfigurations/squidbit/default.nix
@@ -123,7 +123,6 @@ in
   };
 
   systemd.services = {
-
     recyclarr.serviceConfig.LoadCredential = [
       "radarr_api_key:${config.sops.secrets.radarr_api_key.path}"
       "sonarr_api_key:${config.sops.secrets.sonarr_api_key.path}"
