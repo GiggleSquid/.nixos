aad247110e544654a908fb8018fe8f6522f40c01
GiggleSquid <jack.connors@protonmail.com>
2025-11-24 16:37:13 +0000

    feat: add fail2ban for ssh on all machines

--- nix/squid/nixosProfiles/ssh/default.nix
+++ nix/squid/nixosProfiles/ssh/default.nix
@@ -1,4 +1,5 @@
 {
+  programs.ssh.startAgent = true;
   services = {
     openssh = {
       enable = true;
@@ -9,6 +10,22 @@
         PermitRootLogin = "prohibit-password";
       };
     };
+    fail2ban = {
+      enable = true;
+      maxretry = 3;
+      ignoreIP = [
+        "10.0.0.0/8"
+        "185.250.11.100"
+        "185.250.11.244/30"
+        "2a0b:9401:64::/48"
+      ];
+      bantime = "6h";
+      bantime-increment = {
+        enable = true;
+        factor = "1";
+        maxtime = "168h";
+        rndtime = "11m";
+      };
+    };
   };
-  programs.ssh.startAgent = true;
 }
