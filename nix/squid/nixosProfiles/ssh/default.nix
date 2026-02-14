afbeb711f96af6087226a09f9b7f29d874cb4598
GiggleSquid <jack.connors@protonmail.com>
2026-02-14 07:17:47 +0000

    feat: remove fail2ban

--- nix/squid/nixosProfiles/ssh/default.nix
+++ nix/squid/nixosProfiles/ssh/default.nix
@@ -10,22 +10,5 @@
         PermitRootLogin = "prohibit-password";
       };
     };
-    fail2ban = {
-      enable = true;
-      maxretry = 3;
-      ignoreIP = [
-        "10.0.0.0/8"
-        "185.250.11.100"
-        "185.250.11.244/30"
-        "2a0b:9401:64::/48"
-      ];
-      bantime = "6h";
-      bantime-increment = {
-        enable = true;
-        factor = "1";
-        maxtime = "168h";
-        rndtime = "11m";
-      };
-    };
   };
 }
