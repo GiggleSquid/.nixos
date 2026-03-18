73b7ee37a245b9d816f0577c54cf603f4cd527c1
GiggleSquid <jack.connors@protonmail.com>
2026-03-18 00:15:19 +0000

    refactor: clean up wezterm lua config

--- nix/squid/homeProfiles/terminal/default.nix
+++ nix/squid/homeProfiles/terminal/default.nix
@@ -3,36 +3,36 @@
     enable = true;
     extraConfig = # lua
       ''
-        local wezterm = require 'wezterm'
         local config = {}
+        if wezterm.config_builder then
+          config = wezterm.config_builder()
+        end
+
         local gpus = wezterm.gui.enumerate_gpus()
-        -- In newer versions of wezterm, use the config_builder which will
-        -- help provide clearer error messages
-          if wezterm.config_builder then
-            config = wezterm.config_builder()
-          end
-          config = {
-            color_scheme = 'Catppuccin Mocha',
-            hide_tab_bar_if_only_one_tab = true,
-            font = wezterm.font 'Iosevka Term SS14',
-            font_size = 12,
-            front_end = 'WebGpu',
-            window_decorations = 'TITLE | RESIZE',
-            enable_wayland =  true,
-            webgpu_preferred_adapter = {
-              backend = 'Vulkan',
-              device_type = 'DiscreteGpu',
-              name = 'AMD Radeon RX 7800 XT (RADV NAVI32)',
-            }
-          }
-          config.keys = {
+
+        config = {
+          color_scheme = 'Catppuccin Mocha',
+          hide_tab_bar_if_only_one_tab = true,
+          font = wezterm.font 'Iosevka Term SS14',
+          font_size = 12,
+          front_end = 'WebGpu',
+          window_decorations = 'TITLE | RESIZE',
+          enable_wayland =  true,
+          webgpu_preferred_adapter = {
+            backend = 'Vulkan',
+            device_type = 'DiscreteGpu',
+            name = 'AMD Radeon RX 7800 XT (RADV NAVI32)',
+          },
+
+          keys = {
            {
               key = 'Enter',
               mods = 'ALT',
               action = wezterm.action.DisableDefaultAssignment,
             },
-          }
-          return config
+          },
+        }
+        return config
       '';
   };
 }
